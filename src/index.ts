0c22ff995fae63ba6b918bde22971fa0648405c1
Harrison Milbradt <harrison.milbradt@gmail.com>
2020-11-24 21:54:20 -0800

    fix: cleaning up exports

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,6 @@
+export * from './interfaces/rollbar-interceptor-options.interface';
+export * from './interfaces/error-filter.interface';
+export * from './interfaces/rollbar-modules-options.interface';
 export * from './constants';
+export * from './rollbar.interceptor';
 export * from './rollbar.module';
